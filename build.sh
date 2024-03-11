21dd1c44d1328382834f23285b7f887e1760453b
Skyler Ferrante <sjf5462@rit.edu>
2024-03-11 15:36:09 -0400

    Removing setuid stuff

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# requires root
 gcc -o show_open show_open.c
 gcc -o fd_omit fd_omit.c
-# Be careful, the binary this creates is not secure
-sudo chown root show_open
-sudo chmod u+s show_open
--- show_open.c
+++ show_open.c
@@ -4,7 +4,6 @@
 #include<fcntl.h>
 // Show open fds
 int main(){
-	int fd_new = open("./test.log", O_WRONLY | O_APPEND | O_CREAT, 0644);
 	int fd, res;
 	for(fd = 0; fd<10; fd++){
 		res = fcntl(fd, F_GETFD);
